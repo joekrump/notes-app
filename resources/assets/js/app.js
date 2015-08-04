3bf4595f3793c6ef1928779104ad87cf1686774b
Joseph Krump <krump.joseph@gmail.com>
2015-08-03 18:17:14 -0700

    removed duplicate material-ui components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,5 @@
 // let injectTapEventPlugin = require('./material-ui/react-tap-event-plugin');
-let Main = require('./material-ui/main');
+let Main = require('./material-ui/app/main');
 let React = require('react');
 // injectTapEventPlugin();
 
