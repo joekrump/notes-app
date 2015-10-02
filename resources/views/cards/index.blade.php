6e4d88e1a1b7f7286a28043c0167f40f3eb61596
Joseph Krump <krump.joseph@gmail.com>
2015-10-02 09:03:54 -0700

    code clearned up a bit

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -15,7 +15,7 @@
 			<div class="col-sm-4 col-sm-offset-4 text-center">
 				{!! $cards->render() !!}
 			</div>
-			<div id="cards-list" class="col-sm-12">
+			<div id="pagination-content" class="col-sm-12">
 				
 				@foreach($cards as $card)
 					<a class="card col-sm-4" href={{ "/cards/" . $card->id }}>
@@ -56,13 +56,12 @@
 
 @section('javascripts')
 	<script type="text/javascript">
+
 		$(document).ready(function(e, element){
 			var $paginationList = $('.pagination');
 			$paginationList.find('a').click(function(e){
 				e.preventDefault();
 
-				var cards = '';
-				var $cardDiv;
 				var $activePaginationLink = $(this).parents('.pagination').find('li.active');
 				var currentPageNum = $activePaginationLink.text();
 
@@ -72,47 +71,58 @@
 				var $previousPageBtnLink = $activePaginationLink.siblings().first().find('a');
 				var $nextPageBtnLink     = $activePaginationLink.siblings().last().find('a');
 
-				var previousPageURL = $previousPageBtn.attr('href');
-				var nextPageURL     = $nextPageBtn.attr('href');
-
+				var previousPageURL = $previousPageBtnLink.attr('href');
+				var nextPageURL     = $nextPageBtnLink.attr('href');
 
 				$.get($(this).attr('href'), function(response){
-					$.each(response.data, function(index, item){
-
-						$cardDiv = ['<a class="card col-sm-4" href="/cards/"', item.id, '>',
-							'<div class="row">',
-								'<div class="latin col-sm-4">',
-									item.latin,
-								'</div>',
-								'<div class="col-sm-8">',
-									'<div class="row">',
-										'<div class="lesson-number col-sm-12">',
-											'<div class="pull-right">',
-												item.lesson_num,
-											'</div>',
-										'</div>',
-									'</div>',
-									'<div class="english row">',
-										'<div class="col-sm-12">',
-											'<div class="definition">',
-												item.english,
-											'</div>',
-										'</div>',
-										'<div class="col-sm-12">',
-											'<div class="origin">',
-												item.origin,
-											'</div>',
-										'</div>',
-									'</div>',
-								'</div>',
-							'</div>',
-						'</a>'].join('');
-
-						cards += $cardDiv;
-					});
-					$('#cards-list').html(cards);
+					$('#pagination-content').html(makeNewContent(response));
 				});
 			});
 		});
+
+		/**
+		 * Makes the html content that is to be inserted into the page.
+		 * @param  {Object} response The json response
+		 * @return {String}          A string container HTML which is to be inserted.
+		 */
+		function makeNewContent(response){
+			var $cardDiv;
+			var newContent = '';
+			$.each(response.data, function(index, item){
+
+				$cardDiv = ['<a class="card col-sm-4" href="/cards/"', item.id, '>',
+					'<div class="row">',
+						'<div class="latin col-sm-4">',
+							item.latin,
+						'</div>',
+						'<div class="col-sm-8">',
+							'<div class="row">',
+								'<div class="lesson-number col-sm-12">',
+									'<div class="pull-right">',
+										item.lesson_num,
+									'</div>',
+								'</div>',
+							'</div>',
+							'<div class="english row">',
+								'<div class="col-sm-12">',
+									'<div class="definition">',
+										item.english,
+									'</div>',
+								'</div>',
+								'<div class="col-sm-12">',
+									'<div class="origin">',
+										item.origin,
+									'</div>',
+								'</div>',
+							'</div>',
+						'</div>',
+					'</div>',
+				'</a>'].join('');
+
+				newContent += $cardDiv;
+			});
+			console.log(newContent)
+			return newContent;
+		}
 	</script>
 @stop
