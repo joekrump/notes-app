9235c1e22373361ca88b99fd41a06633e875079f
Joseph Krump <krump.joseph@gmail.com>
2015-11-28 16:13:49 -0800

    added commented out number fields for card edit view

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -2,4 +2,7 @@
 
 @section('extras')
 	<input type="text" name="type" class="form-control" value={{ $card_type }} />
+	{{-- Add these fields once ready --}}
+	{{-- <input type="number" name="word_type" class="form-control" value={{ isset($card) ? $card->word_type : null }} />
+	<input type="number" name="word_category" class="form-control" value={{ isset($card) ? $card->word_category : null }} /> --}}
 @stop
