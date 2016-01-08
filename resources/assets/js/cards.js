3e38dc9feb20d73dee8dee144d27fb1dff682972
Joseph Krump <krump.joseph@gmail.com>
2016-01-08 09:06:43 -0800

    added files for displaying react card view

---
+++ resources/assets/js/cards.js
@@ -0,0 +1,6 @@
+let CardPage = require('./material-ui/app/card-page');
+let React = require('react');
+
+(function() {
+  React.render(<CardPage />, document.querySelector('#cards'));
+})();
---
+++ resources/views/cards/react-view.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="_token" content="{{ csrf_token() }}" />
+    </head>
+    <body>
+        <script type="text/javascript" src="{{ asset('js/react-cards.js') }}"></script>
+    </body>
+</html>
