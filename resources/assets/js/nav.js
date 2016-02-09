acfe6f8af569ca6c1e910ac4f199c6c400d223bf
Joseph Krump <krump.joseph@gmail.com>
2016-02-09 14:37:47 -0800

    updates to styling for notes index list

--- resources/assets/js/nav.js
+++ resources/assets/js/nav.js
@@ -3,5 +3,10 @@ let TopLeftNav = require('./material-ui/app/top-left-nav');
 let ReactDOM = require('react-dom');
 
 (function() {
-  ReactDOM.render(<TopLeftNav />, document.querySelector('#react-nav'));
+	var navRight = (
+		<div key="top-right-nav">
+		  <a href='/notes/new' key="new-card-button-top" className="btn btn-success pull-right">New Note</a>
+		</div>
+	);
+  ReactDOM.render(<TopLeftNav navRight={navRight}/>, document.querySelector('#react-nav'));
 })();
