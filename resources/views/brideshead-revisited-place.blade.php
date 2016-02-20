17de24f3e1859aa8927a9ed9f214304a8f3ae919
Joseph Krump <krump.joseph@gmail.com>
2016-02-20 14:02:08 -0800

    auto pathing working and basic adding of locaiton names to list working

--- resources/views/brideshead-revisited-place.blade.php
+++ resources/views/brideshead-revisited-place.blade.php
@@ -5,10 +5,19 @@
 		body {
 			overflow: hidden;
 		}
+		#location-list {
+			position: fixed;
+			top: 0;
+			left: 0;
+			color: white;
+		}
 	</style>
 @stop
 
 @section('content')
+	<ol id="location-list">
+
+	</ol>
   <canvas resize="true" id="paper"></canvas>
 @stop
 
@@ -43,61 +52,88 @@
 			var currentPosition = 0;
 			var cloneCircle;
 			var length;	
-			var amount = 0.2; // controls the speed at which the movement will occur
+			var amount = 0.25; // controls the speed at which the movement will occur
 			var tool = new Tool();
+			var numpathLocations = 0;
+			var pathLocations = [];
+			var listpathLocation;
 
-			function drawLocations(){
+			function drawpathLocations(){
 				var timeDefaults = {
-					center: [400, 400],
-				 	radius: 400
+					center: [250, 850],
+				 	radius: 200,
+				 	data: {
+				 		name: 'test'
+				 	}
 				};
 
-				var primaryLocationDefaults = {
+				var primarypathLocationDefaults = {
 					center: [0, 0],
 				 	radius: 100
 				};
 
-				var secondaryLocationDefaults = {
+				var secondarypathLocationDefaults = {
 					center: [0, 0],
 				 	radius: 50
 				};
 
-				var tertiaryLocationDefautls = {
+				var tertiarypathLocationDefautls = {
 					center: [0, 0],
 				 	radius: 25
 				};
 
 				// add present
 				timeDefaults.fillColor = 'lightgreen';
-				new Path.Circle(timeDefaults);
+				pathLocations.push(new Path.Circle(timeDefaults));
+				numpathLocations++;
 				// add past
 				timeDefaults.fillColor = 'lightgrey';
-				timeDefaults.center = [1500,400];
-				new Path.Circle(timeDefaults);
+				timeDefaults.center = [1400,540];
+				timeDefaults.radius = 800;
+				pathLocations.push(new Path.Circle(timeDefaults));
+				numpathLocations++;
 				
 				// add Brideshead
 				// 
 				// Past
-				primaryLocationDefaults.fillColor = 'forestgreen';
-				primaryLocationDefaults.center = [1300,400];
-				new Path.Circle(primaryLocationDefaults);
+				primarypathLocationDefaults.fillColor = 'forestgreen';
+				primarypathLocationDefaults.center = [1300,400];
+
+				pathLocations.push(new Path.Circle(primarypathLocationDefaults));
+				numpathLocations++;
 				// Present
-				primaryLocationDefaults.center = [200,400];
-				new Path.Circle(primaryLocationDefaults);
+				primarypathLocationDefaults.center = [200,400];
+				pathLocations.push(new Path.Circle(primarypathLocationDefaults));
+				numpathLocations++;
 
 				// add Oxford
 				// 
 				// Past
-				primaryLocationDefaults.fillColor = 'steelblue';
-				primaryLocationDefaults.center = [1700,400];
-				new Path.Circle(primaryLocationDefaults);
+				primarypathLocationDefaults.fillColor = 'steelblue';
+				primarypathLocationDefaults.center = [1700,400];
+				pathLocations.push(new Path.Circle(primarypathLocationDefaults));
+				numpathLocations++;
 				// Present
 				// 
+				
+
 				insertText();
 			}
 
+			var plotPoints = [
+				(new Point(400, 600)),
+				(new Point(400, 500)),
+				(new Point(1000, 500)),
+				(new Point(700, 600)),
+				(new Point(1400, 1000)),
+				(new Point(700, 600)),
+				(new Point(700, 200)),
+				(new Point(1400,540)),
+				(new Point(200, 500))
+			];
+
 			function insertText(){
-					// text for Past location circle
+					// text for Past pathLocation circle
 					var pastText = new PointText({
 						point: [1500,100],
 				    content: 'Past',
@@ -107,9 +143,9 @@
 				    fontSize: '1.5rem',
 				    justification: 'center'
 					});
-					// text for Present location circle
+					// text for Present pathLocation circle
 					var presentText = new PointText({
-						point: [400, 100],
+						point: [250, 750],
 				    content: 'Present',
 				    fillColor: 'black',
 				    fontFamily: 'Tahoma',
@@ -144,16 +180,19 @@
 					bridesheadText.point = [200, 400];
 					// draw for present
 					new PointText(bridesheadText);
-
 			}
 	
-			drawLocations();
-			
+			drawpathLocations();
+			// set a fade time for path segments
+			// 	Once 4 new pathLocations have been hit then fade the one that occurred 4 ago.
+			// 
+			// create a list
+
 			segment1 = new Segment(new Point(400, 600), zeroZero, zeroZero);
 			segment2 = new Segment(new Point(400, 601), zeroZero, zeroZero);
 
 			bridesheadPath.add(segment1, segment2);
-			bridesheadPath.strokeColor ='rgba(0,0,0,0.0)';
+			bridesheadPath.strokeColor = 'rgba(0,0,0,0)';
 			bridesheadPath.strokeWidth = 4;
 			bridesheadPath.closed = false;
 			bridesheadPath.fullySelected = false;
@@ -161,7 +200,7 @@
 			
 			project.activeLayer.addChild(charlesDot)
 			
-			//calculate the length of one part of the path
+			// calculate the length of one part of the path
 			length = bridesheadPath.length / amount;
 
 			// animate the circle, moving from position to position along the bridesheadPath
@@ -173,8 +212,13 @@
 		    	++currentPosition; // Move charles Dot ahead by one position
 		    	if(currentPosition % 5 == 0){
 		    		// Drop a 'tomato' coloured circle every fifth position change
-		    		createPathCircle(carposition);
+		    		createPathCircle(carposition, 'tomato', 5);
 		    		charlesDot.bringToFront();
+		    	} else {
+		    		createPathCircle(carposition, 'red', 3);
+		    	}
+		    	if((listpathLocation = isInApathLocation(charlesDot)) !== false){
+		    		insertListItem(listpathLocation);
 		    	}
 		    } else {
 		    	// Keep charlesDot at its current position
@@ -182,25 +226,59 @@
 		    }
 			}
 
-			function createPathCircle(carposition){
-    		var pathCircle = new Path.Circle(carposition, 5);
-    		pathCircle.fillColor = 'tomato';
-    		pathCircle.shadowColor = new Color(0, 0, 0);
-		    pathCircle.shadowBlur = 4;
-		    pathCircle.shadowOffset = new Point(1, 1);
+			function createPathCircle(carposition, color, size){
+    		var pathCircle = new Path.Circle(carposition, size);
+    		pathCircle.fillColor = color;
+    		if(size === 5){
+    			pathCircle.shadowColor = new Color(0, 0, 0);
+		    	pathCircle.shadowBlur = 4;
+		    	pathCircle.shadowOffset = new Point(1, 1);
+    		}
+    		
 		    return pathCircle;
 			}
-			
-		  // var previousLength;
-			paper.tool.onMouseDown = function(event) {
-        x = event.event.offsetX;
-        y = event.event.offsetY;
-        lastPosition = new Point(x, y);
-        bridesheadPath.add(new Segment(lastPosition, new Point(0,0), new Point(0,0)));
 
-        amount = bridesheadPath.length / length;
-        // previousLength = length;
-        length = bridesheadPath.length / amount;
+			function insertListItem(pathLocation){
+				$('#location-list').append('<li>' + pathLocation.data.name+ '</li>');
+
+			}
+
+
+			/**
+			 * Check if the pathLocation of Charles is within a pathLocation.
+			 * @param  {[type]}  charlesDot [description]
+			 * @return {Boolean}            [description]
+			 */
+			function isInApathLocation(charlesDot){
+				for(var j = 0; j < numpathLocations; j++){
+					if(charlesDot.intersects(pathLocations[j])){
+						return pathLocations[j];
+					}
+				}
+				return false;
+			}
+
+			/**
+			 * If a pathLocation has a parent return true, else false
+			 * @param  {[type]} pathLocation [description]
+			 * @return {[type]}          [description]
+			 */
+			function pathLocationHasParent(pathLocation){
+
+			}
+
+			paper.tool.onMouseDown = function(event) {
+        // x = event.event.offsetX;
+        // y = event.event.offsetY;
+        // lastPosition = new Point(x, y);
+        for(var i = 0; i < plotPoints.length; i++){
+        	bridesheadPath.add(new Segment(plotPoints[i], new Point(0,0), new Point(0,0)));
+
+        	amount = bridesheadPath.length / length;
+        	
+        	length = bridesheadPath.length / amount;
+        }
+        
 
         paper.view.attach('frame', onFrame);
 			}
